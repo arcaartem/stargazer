c598d160d1dd481687564b7c2ef7bcdeadbbd9a5
Arca <arca.artem@gmail.com>
2025-02-17 17:31:02 +0000

    Add stories for the components

---
+++ src/stories/RepoCard.stories.svelte
@@ -0,0 +1,31 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import RepoCard from '../lib/components/RepoCard.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Stargazer/Components/RepoCard',
+		component: RepoCard,
+		tags: ['autodocs'],
+		argTypes: {
+			repo: { control: 'object' }
+		}
+	});
+</script>
+
+<Story
+	name="Default"
+	args={{
+		repo: {
+			id: 123,
+			name: 'Repo Name',
+			description: 'Repo Description',
+			html_url: 'https://github.com',
+			language: 'JavaScript',
+			stargazers_count: 100,
+			updated_at: '2025-05-01T00:00:00Z',
+			owner: {
+				login: 'owner'
+			}
+		}
+	}}
+/>
